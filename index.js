c86d2a1ad4f0e149ec1033b803ee55044ddd9403
Matheus Ribeiro <matheus.tex02@gmail.com>
2025-01-07 20:34:22 -0300

    ping server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
+const router = express.Router();
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +21,10 @@ app.use("/auth", usersRouter);
 const likesRouter = require("./routes/Likes");
 app.use("/like", likesRouter);
 
+router.get("/ping", (req, res) => {
+  res.json({ message: "Servidor está online!" });
+});
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
@@ -33,3 +38,4 @@ db.sequelize.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   });
+  module.exports = router;
